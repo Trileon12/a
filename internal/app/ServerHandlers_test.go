563230eda3a768596de5a884b5c80f30d57c291a
Igor Petrov <igor4petrov@gmail.com>
2022-01-08 01:33:03 +0300

    inc 7

--- internal/app/ServerHandlers_test.go
+++ internal/app/ServerHandlers_test.go
@@ -154,45 +154,6 @@ func TestGetShortURL(t *testing.T) {
 	}
 }
 
-func TestShortURL(t *testing.T) {
-
-	conf := config.New()
-	s := storage.New(&conf.Storage)
-	application := app.New(&conf.App, s)
-
-	tests := []tstRequest{
-		{
-
-			nameTest: "Tst not found URL",
-			request: request{
-				method:      http.MethodGet,
-				url:         host + "/qGHrty",
-				originalURL: "www.google.com",
-			},
-			want1: want{
-
-				statusCode: http.StatusNotFound,
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.nameTest, func(t *testing.T) {
-
-			request := httptest.NewRequest(tt.request.method, tt.request.url, strings.NewReader(tt.request.originalURL))
-			result := SendRequest(request, application.GetFullURLByShortURL)
-
-			assert.Equal(t, tt.want1.statusCode, result.StatusCode)
-
-			defer result.Body.Close()
-			//if err != nil {
-			//	assert.Error(t, err, "Error on read body after get short link")
-			//}
-
-		})
-	}
-}
-
 func SendRequest(request *http.Request, f func(writer http.ResponseWriter, request *http.Request)) *http.Response {
 	w := httptest.NewRecorder()
 	h := http.HandlerFunc(f)
